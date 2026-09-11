ce93fa61a9da7253f144384de886b4f6f6746a31
maddy <madisonroche01@gmail.com>
2026-09-11 15:17:46 +0100

    Completed Task Lab 1 - fixed small css issues missed

--- tasky/src/components/Task.jsx
+++ tasky/src/components/Task.jsx
@@ -2,7 +2,7 @@ const Task = (props) => {
     
     return (
         <div className="card">
-            <p>{props.title}</p>
+            <p className="title">{props.title}</p>
             <p>Due: {props.deadline}</p>
             <p className="description">{props.description}</p>
         </div>
