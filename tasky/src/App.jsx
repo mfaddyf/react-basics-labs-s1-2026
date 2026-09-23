190bd7d10cc05492b90439daaf67ca5d4d4c0b16
mfaddyf <madisonroche01@gmail.com>
2026-09-23 13:32:58 +0100

    delete a task

--- tasky/src/App.jsx
+++ tasky/src/App.jsx
@@ -18,6 +18,12 @@ function App() {
     console.log(`${taskIndex} ${tasks[taskIndex].done}`);
   }
 
+  const deleteHandler = (taskIndex) => {
+    const tasks = [...taskState.tasks];
+    tasks.splice(taskIndex, 1);
+    setTaskState({ tasks });
+  }
+
   return (
     <div className="container">
       <h1>Tasky</h1>
@@ -31,6 +37,7 @@ function App() {
           priority={task.priority}
           done={task.done}
           markDone={() => doneHandler(index)}
+          deleteTask = {() => deleteHandler(index)}
         />
       ))}
 
